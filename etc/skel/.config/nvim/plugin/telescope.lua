a050c7a4c6581a2a70e7b1aea61e3ef83d184e9f
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-05-31 17:38:40 -0400

    telescope.lua: better config

--- etc/skel/.config/nvim/plugin/telescope.lua
+++ etc/skel/.config/nvim/plugin/telescope.lua
@@ -2,10 +2,44 @@ local function map(mode, lhs, rhs)
     vim.keymap.set(mode, lhs, rhs, {noremap = true})
 end
 
-local scope = require("telescope.builtin")
-map("n", "<leader>fb", scope.buffers)
-map("n", "<leader>ff", scope.find_files)
-map("n", "<leader>fg", scope.live_grep)
-map("n", "<leader>fh", scope.help_tags)
-map("n", "<leader>fm", scope.man_pages)
-map("n", "<leader>fr", scope.resume)
+require("telescope").setup({
+    defaults = {
+        borderchars = {
+            prompt  = { "─", "│", "─", "│", "╭", "╮", "┤", "├" },
+            results = { " ", "│", "─", "│", "│", "│", "╯", "╰" },
+            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        },
+        layout_config = {
+            horizontal = {
+                preview_width = 0.6,
+                prompt_position = "top",
+            },
+        },
+        multi_icon = "",
+        prompt_prefix = "   ",
+        results_title = false,
+        selection_caret = "▌ ",
+        sorting_strategy = "ascending",
+    },
+    pickers = {
+        buffers = {
+            layout_config = {
+                width = 50,
+            },
+            mappings = {
+                i = { ["<C-x>"] = "delete_buffer" },
+                n = { ["<C-x>"] = "delete_buffer" },
+            },
+            preview = false,
+            results_title = "Ctrl+X to close buffer",
+        },
+    },
+})
+
+local builtin = require("telescope.builtin")
+map("n", "<leader>fb", builtin.buffers)
+map("n", "<leader>ff", builtin.find_files)
+map("n", "<leader>fg", builtin.live_grep)
+map("n", "<leader>fh", builtin.help_tags)
+map("n", "<leader>fm", builtin.man_pages)
+map("n", "<leader>fr", builtin.resume)
