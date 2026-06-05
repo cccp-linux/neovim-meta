6103854a4b506cfe445d0c4fef16dde41a52574f
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-06-04 21:10:46 -0400

    oil.lua: new oil.nvim needs explicit border

--- etc/skel/.config/nvim/plugin/oil.lua
+++ etc/skel/.config/nvim/plugin/oil.lua
@@ -5,8 +5,9 @@ require("oil").setup({
         -- "size",
         -- "mtime",
     },
-    watch_for_changes = true,
+    confirmation = {border = "rounded"},
     view_options = {show_hidden = true},
+    watch_for_changes = true,
 })
 
 vim.keymap.set("n", "-", "<cmd>Oil<cr>", {noremap = true})
